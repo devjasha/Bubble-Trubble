a89f628b44b4ceecee225cbef4299212f13e23e9
jasha.chec <jasha@chec.de>
2023-01-15 09:26:17 +0100

    feat(homepage): add components

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -1,42 +1,155 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'dart:async';
 
 import 'button.dart';
 import 'player.dart';
+import 'missile.dart';
+import 'ball.dart';
 
 class HomePage extends StatefulWidget {
   @override
   _HomePageState createState() => _HomePageState();
 }
 
-class _HomePageState extends State<HomePage> {
-  double playerX = 0;
+enum direction { LEFT, RIGHT }
 
-  double missileX = 0;
-  double missileY = 1;
+class _HomePageState extends State<HomePage> {
+  static double playerX = 0;
+
+  double missileX = playerX;
+  double missileHeight = 10;
+  bool midshot = false;
+
+  double ballX = 0.5;
+  double ballY = 0;
+  var ballDirection = direction.LEFT;
+
+  void startGame() {
+    double time = 0;
+    double height = 0;
+    double velocity = 60;
+
+    Timer.periodic(Duration(milliseconds: 10), (timer) {
+      height = -5 * time * time + velocity * time;
+
+      if (height < 0) {
+        time = 0;
+      }
+
+      setState(() {
+        ballY = heightToPosition(height);
+      });
+
+
+      if(ballX - 0.005 < -1) {
+        ballDirection = direction.RIGHT;
+      } else if (ballX + 0.005 > 1) {
+        ballDirection = direction.LEFT;
+      }
+
+      if (ballDirection == direction.LEFT) {
+        setState(() {
+          ballX -= 0.005;
+        });
+      } else if (ballDirection == direction.RIGHT) {
+        setState(() {
+          ballX += 0.005;
+        });
+      }
+
+      if (playerDies()) {
+        timer.cancel();
+        _showDialog();
+      }
+
+      time += 0.1;
+    });
+  }
+
+  void _showDialog() {
+    showDialog(
+      context: context, 
+      builder: (BuildContext context) {
+      return AlertDialog(
+        backgroundColor: Colors.grey[800],
+        title: const Center(
+          child: Text(
+            "You dead",
+            style: TextStyle(
+              color: Colors.white,
+            ),
+          ),
+        )
+      );
+    });
+  }
 
   void moveLeft() {
     setState(() {
       if(playerX - 0.1 < -1) {
-        
+        // do nothing
       } else {
         playerX -= 0.1;  
+      }
+
+      if (!midshot) {
+        missileX = playerX;
       }
     });
   }
   void moveRight() {
     setState(() {
       if(playerX + 0.1 > 1) {
-        
+        // do nothing
       } else {
         playerX += 0.1;  
       }
+      missileX = playerX;
     });
   }
   void fireMissile() {
-    Timer.periodic(Duration(milliseconds: 100), (timer)) {
-      
-    })
+    if (midshot == false) {
+      Timer.periodic( const Duration(milliseconds: 20), (timer) {
+
+        midshot = true;
+
+        setState(() {
+          missileHeight += 10;
+        });
+
+        if (missileHeight > MediaQuery.of(context).size.height * 3 / 4) {
+          resetMissile();
+          timer.cancel();
+        }
+
+        if (ballY > heightToPosition(missileHeight) && (ballX - missileX).abs() < 0.03) {
+          resetMissile();
+          ballX = 5;
+          timer.cancel();
+        }
+      });
+    }
+  }
+
+  double heightToPosition(double height) {
+    double totalHeight = MediaQuery.of(context).size.height * 3 / 4;
+    double position = 1 - 2 * height / totalHeight;
+    return position;
+  }
+
+  void resetMissile() {
+    missileX = playerX;
+    missileHeight = 0;
+    midshot = false;
+  }
+
+  bool playerDies() {
+    if ((ballX - playerX).abs() < 0.05 && ballY > 0.95) {
+      return true;
+    } else {
+      return false;
+    }
   }
 
   @override
@@ -64,9 +177,17 @@ class _HomePageState extends State<HomePage> {
               child: Center(
                 child: Stack(
                   children: [
+                    MyBall(
+                      ballX: ballX, 
+                      ballY: ballY
+                    ),
+                    MyMissile(
+                      height: missileHeight,
+                      missileX: missileX,
+                    ),
                     MyPlayer(
                       playerX: playerX,
-                    )
+                    ),
                   ],
                 ),
               )
@@ -78,6 +199,10 @@ class _HomePageState extends State<HomePage> {
               child: Row(
                 mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                 children: [
+                  MyButton(
+                    icon: Icons.play_arrow,
+                    function: startGame,
+                  ),
                   MyButton(
                     icon: Icons.arrow_back,
                     function: moveLeft,
